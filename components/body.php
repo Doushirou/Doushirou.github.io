64aa7f726104d99ab0a6c7f273b92dbcee168e6c
Chu Quang Anh <designlikeaboss@gmail.com>
2014-07-29 10:36:36 +0700

    testing
    will github work with php?

---
+++ components/body.php
@@ -0,0 +1,9 @@
+
+<body>
+<h1>Hello PHP</h1>
+<p>I'm hosted with GitHub Pages. </p>
+</body>
+
+
+
+</html>
---
+++ components/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+	<title>Ha Thanh Rubber Industrial Co.,LTD</title>
+	<meta name="viewport" content="width=device-width, user-scalable=no">
+	<meta name="description" content="Công ty trách nhiệm hữu hạn công nghiệp cao su Hà Thành" />
+	<meta name="keywords" content="Rubber, Hush Firewood, Wood Chip, Viên Nén Củi, Củi Dăm, Cao Su Tự Nhiên" />
+	<meta name="author" content="Ha Thanh" />
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
+</head>
